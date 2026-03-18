df3569a67e933d28747bb98ff1372684f453d570
mh110107-maker <mh110107@gmail.com>
2026-03-18 12:02:33 +0900

    수요일 수업
    첫날 과제

---
+++ Assets/Script/CharacterMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    public float spd = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        float v = Input.GetAxis("Vertical");
+
+        // Vector3 direct = Vector3.right * h + Vector3.up * v;
+        Vector3 derect = new Vector3(h, v, 0);
+
+        //transform.Translate(derect * spd * Time.deltaTime);
+        transform.position = transform.position + derect * spd * Time.deltaTime;
+
+    }
+}
